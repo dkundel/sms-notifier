b22c207b3009b2b3898f750696ef5f2b94abbf6e
Dominik Kundel <dominik.kundel@gmail.com>
2016-07-12 11:59:44 +0200

    Fixing auth

--- auth.js
+++ auth.js
@@ -4,7 +4,7 @@ const config = require('./config');
 module.exports = function (req, res, next) {
   function unauthorized(res) {
     res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
-    return res.sendStatus(401);
+    return res.send(401);
   };
 
   var user = basicAuth(req);
